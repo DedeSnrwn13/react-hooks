ad2f956c97b71f47b5a3fe2a605f070d7dd33779
DedeSnrwn13 <sunarwandede13@gmail.com>
2024-09-21 09:31:27 +0700

    initial commit

---
+++ src/State.jsx
@@ -0,0 +1,13 @@
+const State = () => {
+    let count = 0;
+
+    return (
+        <>
+            <button>-</button>
+            <div>{count}</div>
+            <button>+</button>
+        </>
+    );
+}
+
+export default State;
