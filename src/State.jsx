cd6f319d36c6937d39650fcc8a46734ad3245e96
DedeSnrwn13 <sunarwandede13@gmail.com>
2024-09-21 10:00:03 +0700

    feat: useState

--- src/State.jsx
+++ src/State.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 const State = () => {
-    let count = 0;
+    const [count, setCount] = useState(0);
+    const [value, setValue] = useState('');
+
+    const dec = () => setCount((prev) => (--prev));
+    const inc = () => setCount((prev) => (++prev))
+
+    /** @type {JSX.IntrinsicElements["input"]["onChange"]} */
+    const onInputChange = (evt) => {
+        setValue(evt.target.value);
+    }
 
     return (
         <>
-            <button>-</button>
-            <div>{count}</div>
-            <button>+</button>
+            <div>
+                <button onClick={dec}>-</button>
+                <div>{count}</div>
+                <button onClick={inc}>+</button>
+            </div>
+
+            <input type="text" value={value} onChange={onInputChange} />
         </>
     );
 }
